dfbc1116b4cfebd6320f150d9c359fd4f54fa6a2
Abdul Rafay <rafay_007@outlook.com>
2026-05-02 11:44:21 +0500

    store import error fixed

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 import { Provider } from 'react-redux'
-import store from './Redux/Store.js'
+import store from './Redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
